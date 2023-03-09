c19ef2a7feec72ef50317c4e4406364d3d613a50
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-03-08 16:19:06 -0800

    added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
-        <nav>
-          Nav
-        </nav>
+        <Navbar />
         <section className="hero">
 
         </section>
