6f34ffaebe86951cf3466939e725c68c4182cf65
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-03-08 18:54:55 -0800

    added hero content & styling with overlay

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Navbar from './components/Navbar/Navbar'
 function App() {
   return (
     <div className="App">
-      <header className='App-header'>
-        <Navbar />
-        <section className="hero">
-
-        </section>
-      </header>
+      
+      <div className='overlay'></div>
+      <div className='background-img'>
+        <header className='App-header'>
+          <Navbar />
+          <section className="hero-section">
+              <h1 className='lead'>immersive experiences that deliver</h1>
+          </section>
+        </header>
+      </div>
     </div>
   );
 }
