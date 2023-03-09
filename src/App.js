3a981fd15bbe3bdf1de33ecea04a5939bc0e5195
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-03-09 11:59:47 -0800

    added toggle display function, content, and styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+import { useState } from 'react'
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
+  const [isHamburgerClicked, setIsHamburgerClicked] = useState(false);
+
+  const handleHamburgerClick = () => {
+    setIsHamburgerClicked(!isHamburgerClicked);
+  };
+
   return (
     <div className="App">
       
       <div className='overlay'></div>
       <div className='background-img'>
         <header className='App-header'>
-          <Navbar />
+          <Navbar onHamburgerClick={handleHamburgerClick} />
+          <div className={isHamburgerClicked ? "" : "hide"}>
+          <div className='mobile-links-container'>
+            <ul className="mobile-links">  
+              <li>
+                <a href="/">about</a>
+              </li>
+              <li>
+                <a href="/">careers</a>
+              </li>
+              <li>
+                <a href="/">products</a>
+              </li>
+              <li>
+                <a href="/">support</a>
+              </li>
+            </ul>
+          </div>
+          </div>
           <section className="hero-section">
               <h1 className='lead'>immersive experiences that deliver</h1>
           </section>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,29 +4,35 @@ import './Navbar.css'
 import hamburger from '../../images/icons/icon-hamburger.svg'
 import close from '../../images/icons/icon-close.svg'
 
-function Navbar() {
-  const [isHamburgerClicked, setIsHamburgerClicked] = useState(false)
+function Navbar(props) {
+  const [isHamburgerClicked, setIsHamburgerClicked] = useState(false);
 
-  const handleClick = () => {
-    setIsHamburgerClicked(!isHamburgerClicked)
-  }
+  const handleHamburgerClick = () => {
+    setIsHamburgerClicked(!isHamburgerClicked);
+    props.onHamburgerClick(!isHamburgerClicked);
+  };
 
   return (
     <nav>
-      <a className="logo">LoveSoul</a>
-      <img className='icon-toggle' src={isHamburgerClicked ? close : hamburger} onClick={handleClick} />
+      <a href='/' className="logo">LoveSoul</a>
+      <img
+        className="icon-toggle"
+        src={isHamburgerClicked ? close : hamburger}
+        alt="hamburger icon"
+        onClick={handleHamburgerClick}
+      />
       <ul className="links">  
         <li>
-          <a href="#">about</a>
+          <a href="/">about</a>
         </li>
         <li>
-          <a href="#">careers</a>
+          <a href="/">careers</a>
         </li>
         <li>
-          <a href="#">products</a>
+          <a href="/">products</a>
         </li>
         <li>
-          <a href="#">support</a>
+          <a href="/">support</a>
         </li>
       </ul>
     </nav>
